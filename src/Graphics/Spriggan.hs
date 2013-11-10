9e3781deaeec92686d35ab2117e3889d845a8870
nvd1234@gmail.com <nvd1234@gmail.com>
2013-11-10 21:59:30 +0000

    Added addSprite

--- src/Graphics/Spriggan.hs
+++ src/Graphics/Spriggan.hs
@@ -3,8 +3,10 @@ module Graphics.Spriggan (Spriggan, Sprite, Costume(..)) where
 
 import Codec.Picture
 import Control.Applicative
+import Control.Monad.IO.Class
 import Control.Monad.Trans.State.Strict
 import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
 import Data.Vault.Strict
 
 newtype Spriggan a = Spriggan {getSpriggan :: StateT Vault IO a}
@@ -15,13 +17,26 @@ data Costume = Costume {
   costumeCentre :: (Int, Int)
   }
 
-data SpriteInternal {
+data SpriteInternal = SpriteInternal {
   costumes :: Seq Costume,
   position :: (Int, Int),
   rotation :: Double,
   visible :: Bool
   }
 
+defSpriteInternal :: SpriteInternal
+defSpriteInternal = SpriteInternal {
+  costumes = Seq.empty,
+  position = (0,0),
+  rotation = 0,
+  visible = False
+  }
+
 newtype Sprite = Sprite {getSprite :: Key SpriteInternal}
 
-
+addSprite :: Spriggan Sprite
+addSprite = Spriggan $ do
+  k <- liftIO newKey
+  modify $ insert k defSpriteInternal
+  return $ Sprite k
+  
